cf3be5304f448e0c47a45820e6c5e2017cd27219
ewanreis <87541930+ewanreis@users.noreply.github.com>
2021-12-08 16:38:06 +0000

    Update
    Character Model + Fixed Camera + Updated Idle Animation

--- fmp-pre/Assets/Scripts/playerController.cs
+++ fmp-pre/Assets/Scripts/playerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 6f, turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal"),  vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal,0,vertical).normalized;
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y, angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            
+            transform.rotation = Quaternion.Euler(0f,angle, 0f);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized*speed*Time.deltaTime);
+        }
     }
 }
