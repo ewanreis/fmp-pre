d9fe81f2ff38bef78130bfbbc9c6fcd82847d6ba
ewanreis <87541930+ewanreis@users.noreply.github.com>
2022-02-07 16:06:50 +0000

    Optimization

--- fmp-pre/Assets/Scripts/Helper.cs
+++ fmp-pre/Assets/Scripts/Helper.cs
@@ -22,10 +22,6 @@ public class Helper
     public void PlayerState()
     {
         groundedPlayer = controller.isGrounded;
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-        {
-            
-        }
     }
     public void MovePlayer()
     {
@@ -33,11 +29,10 @@ public class Helper
         move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero) gameObject.transform.forward = move;
+        //if (move != Vector3.zero) gameObject.transform.forward = move;
 
         // Changes the height position of the player..
         if (Input.GetButtonDown("Jump") && groundedPlayer) playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-            
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
