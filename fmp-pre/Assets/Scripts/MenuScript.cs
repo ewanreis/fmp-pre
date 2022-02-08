d7ebcf1ecea3633c1cd710b3194631cd5a1da8dd
ewanreis <87541930+ewanreis@users.noreply.github.com>
2022-02-08 16:11:53 +0000

    Menu Sounds

--- fmp-pre/Assets/Scripts/MenuScript.cs
+++ fmp-pre/Assets/Scripts/MenuScript.cs
@@ -2,9 +2,60 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
+    private bool buttonAvailable = true, isCreepy = false;
     public GameObject settingsMenu;
-    void Start() => settingsMenu.SetActive(false);
-    public void ContinueGame(int level) => SceneManager.LoadScene(level);
-    public void StartGame() => SceneManager.LoadScene("SampleScene");
-    public void QuitGame() => Application.Quit();
+    public AudioClip[] clips;
+    public AudioSource source;
+
+    private int level = 1;
+
+    enum Sounds
+    {
+        continueHover, // 0
+        continueClick, // 1
+        clickDeny, // 2
+        startClick, // 3
+        hoverDeny, // 4
+        startHover, // 5
+        quitClick, // 6
+        menuMusic, // 7
+        menuMusicCreepy // 8
+    }
+
+    void Start() 
+    {
+        settingsMenu.SetActive(false);
+        Sounds music = (isCreepy) ? Sounds.menuMusic : Sounds.menuMusicCreepy;
+        source.PlayOneShot(clips[(int)music],0.2f);
+    }
+
+    public void ContinueGame() 
+    {
+        if (buttonAvailable)
+        {
+            source.PlayOneShot(clips[(int)Sounds.continueClick]);
+            Invoke("LoadScene", 1.5f);
+        }
+        else
+            source.PlayOneShot(clips[(int)Sounds.clickDeny]);
+    }
+
+    public void StartGame() 
+    {
+        level = 2;
+        source.PlayOneShot(clips[(int)Sounds.startClick]);
+        Invoke("LoadScene", 1.5f);
+    }
+
+    public void QuitGame()
+    {
+        source.PlayOneShot(clips[(int)Sounds.quitClick]);
+        Invoke("EndGame", 1f);
+    }
+
+    public void ContinueHover() => source.PlayOneShot(clips[(buttonAvailable) ? (int)Sounds.continueHover : (int)Sounds.hoverDeny]);
+    public void StartHover() => source.PlayOneShot(clips[(int)Sounds.startHover]);
+    public void EndGame() => Application.Quit();
+
+    private void LoadScene() => SceneManager.LoadScene(level); 
 }
