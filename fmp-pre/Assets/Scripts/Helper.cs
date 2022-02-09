ebb34de3848cc5bcb191b85b9d3bb574b1462ba8
ewanreis <87541930+ewanreis@users.noreply.github.com>
2022-02-09 23:01:48 +0000

    Menu Update and Working Movement

--- fmp-pre/Assets/Scripts/Helper.cs
+++ fmp-pre/Assets/Scripts/Helper.cs
@@ -4,34 +4,6 @@ using UnityEngine;
 
 public class Helper
 {
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float playerSpeed = 2.0f, jumpHeight = 1.0f, gravityValue = -9.81f;
 
-    public GameObject player;
-    private void Start()
-    {
-        //controller = GetComponent<CharacterController>();
-    }
 
-    public void MovePlayer()
-    {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-            playerVelocity.y = 0f;
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-            player.transform.forward = move;
-
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-    }
 }
--- fmp-pre/Assets/Scripts/playerController.cs
+++ fmp-pre/Assets/Scripts/playerController.cs
@@ -2,52 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class playerController : MonoBehaviour
-{   public CharacterController controller;
+{
+    public CharacterController controller;
     public Transform cam, groundCheck, playerBody;
-    public float speed = 6, gravity = -9.81f, jumpHeight = 3, turnSmoothTime = 0.1f, groundDistance = 0.4f, mouseSensitivity;
     public LayerMask groundMask;
-    float xRotation = 0f;
-    Vector3 velocity;
-    bool isGrounded;
-    float turnSmoothVelocity;
+    public float speed = 6, gravity = -9.81f, jumpHeight = 3, turnSmoothTime = 0.1f, groundDistance = 0.4f, mouseSensitivity;
 
-    void Update()
+    private Vector2 mouseInput, movementInput;
+    private Vector3 moveDir, velocity, direction;
+    private float xRotation, turnSmoothVelocity, targetAngle, angle;
+    private bool isGrounded;
+
+    void FixedUpdate()
     {
-        
         #region Jumping
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0)
-            velocity.y = -2f;
-        if (Input.GetButtonDown("Jump") && isGrounded)
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-        #endregion
+        if (isGrounded && velocity.y < 0) velocity.y = -2f;
+        if (Input.GetButtonDown("Jump") && isGrounded) velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        #endregion // Checks if the player is grounded, then jumps when assigned Jump button is pressed
 
         #region Gravity
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-        #endregion
+        #endregion // Applies Gravity value at constant rate
 
         #region Walking
-        float horizontal = Input.GetAxisRaw("Horizontal"), vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        direction = new Vector3(movementInput.x, 0f, movementInput.y).normalized;
         if(direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-        #endregion
+        #endregion // Moves the player
 
         #region Turning
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime, mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
-        //transform.rotation = Quaternion.LookRotation(dir);
-        playerBody.Rotate(Vector3.up * mouseX);
-        #endregion
-        
+        mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseInput.x;
+        playerBody.transform.rotation = Quaternion.Euler(0, xRotation, 0);
+        #endregion // Gets the input from the mouse and turns the player
     }
 }
